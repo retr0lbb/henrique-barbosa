1ca2230806d3cde252761ae689520ce8e01f2f12
retr0lbb <retr0lbb@gmail.com>
2025-11-18 21:59:03 -0300

    feat: add style animation

--- src/components/scrollingHistory/scroller.tsx
+++ src/components/scrollingHistory/scroller.tsx
@@ -1,12 +1,14 @@
-"use client"
-import { motion } from "framer-motion"
+"use client";
+import { motion } from "framer-motion";
 
-
-interface ScrollerProps{ 
+interface ScrollerProps { 
     children: React.ReactNode
 }
+
 export function Scroller(props: ScrollerProps){
-    return <motion.div className="w-full min-h-dvh">
-        {props.children}
-    </motion.div>
+    return (
+        <motion.div className="w-full min-h-dvh">
+            {props.children}
+        </motion.div>
+    );
 }
--- src/sections/about-section.tsx
+++ src/sections/about-section.tsx
@@ -5,13 +5,9 @@ export function AboutSection() {
   return (
     <section className="w-full min-h-dvh bg-zinc-950">
       <Scroller>
-        <ScrollSection year={2017} children="aa" />
+        <ScrollSection year={2017} children="aa" isOnStart />
         <ScrollSection year={2018} children="aa" />
-        <ScrollSection year={2019} children="aa" />
-        <ScrollSection year={2020} children="aa" />
-        <ScrollSection year={2021} children="aa" />
-        <ScrollSection year={2022} children="aa" />
-
+        <ScrollSection year={2022} children="aa" isOnEnd/>
       </Scroller>
     </section>
   );
